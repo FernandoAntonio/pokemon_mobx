901b1ba0bac8cfc241e288660e9edeb1280a3859
FernandoAntonio <30207066+fernandoantonio@users.noreply.github.com>
2020-11-14 00:09:07 -0300

    Fixed layout error that only occours in release mode
    Signed-off-by: Fernando Antonio <30207066+fernandoantonio@users.noreply.github.com>

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -8,7 +8,7 @@ class AppWidget extends StatelessWidget {
       navigatorKey: Modular.navigatorKey,
       title: 'Flutter Slidy',
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey[800],
+        primarySwatch: Colors.blueGrey,
         accentColor: Colors.blueGrey[800],
       ),
       initialRoute: '/',
