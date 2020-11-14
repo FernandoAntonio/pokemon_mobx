ab874fbf6009b04e81329a3263f3e9ac5eb7aadb
FernandoAntonio <30207066+fernandoantonio@users.noreply.github.com>
2020-11-13 23:50:50 -0300

    Fixed layout error that only occours in release mode
    Signed-off-by: Fernando Antonio <30207066+fernandoantonio@users.noreply.github.com>

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -35,44 +35,41 @@ class _HomePageState extends ModularState<HomePage, HomeController> {
         title: Text('All Pokemons'),
       ),
       body: Column(
-        mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Expanded(
-            child: Observer(
-              builder: (_) {
-                if (controller.pokemons == null || controller.pokemons.value == null) {
-                  return Center(child: CircularProgressIndicator());
-                } else if (controller.pokemons.error != null) {
-                  return Text('Ocorreu um erro inesperado');
-                } else
-                  return ListView.builder(
-                    itemCount: controller.pokemons.value.results.length,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        height: 80.0,
-                        child: Card(
-                          margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
-                          child: Center(
-                            child: ListTile(
-                              leading: Image.network(
-                                  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png'),
-                              title: Row(
-                                children: [
-                                  Text((index + 1).toString(),
-                                      style: TextStyle(color: Colors.grey[400])),
-                                  SizedBox(width: 8.0),
-                                  Text(formatPokemonNames(
-                                      controller.pokemons.value.results[index].name)),
-                                ],
-                              ),
+          Observer(
+            builder: (_) {
+              if (controller.pokemons == null || controller.pokemons.value == null) {
+                return Center(child: CircularProgressIndicator());
+              } else if (controller.pokemons.error != null) {
+                return Text('Ocorreu um erro inesperado');
+              } else
+                return ListView.builder(
+                  itemCount: controller.pokemons.value.results.length,
+                  itemBuilder: (context, index) {
+                    return Container(
+                      height: 80.0,
+                      child: Card(
+                        margin: EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+                        child: Center(
+                          child: ListTile(
+                            leading: Image.network(
+                                'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png'),
+                            title: Row(
+                              children: [
+                                Text((index + 1).toString(),
+                                    style: TextStyle(color: Colors.grey[400])),
+                                SizedBox(width: 8.0),
+                                Text(formatPokemonNames(
+                                    controller.pokemons.value.results[index].name)),
+                              ],
                             ),
                           ),
                         ),
-                      );
-                    },
-                  );
-              },
-            ),
+                      ),
+                    );
+                  },
+                );
+            },
           ),
         ],
       ),
