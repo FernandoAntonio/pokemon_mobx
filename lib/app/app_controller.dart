4bf6cdcabce321a6c717367577a513727c8dae0c
FernandoAntonio <30207066+FernandoAntonio@users.noreply.github.com>
2021-04-22 12:04:24 -0300

    Update app_controller.dart
    Removed old code

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -6,21 +6,4 @@ part 'app_controller.g.dart';
 @Injectable()
 class AppController = _AppControllerBase with _$AppController;
 
-abstract class _AppControllerBase with Store {
-  @observable
-  String nomeCliente = 'José';
-
-  @observable
-  String sobrenomeCliente = 'da Silva';
-
-  @observable
-  int value = 0;
-
-  @computed
-  String get nomeCompleto => nomeCliente.trim().toUpperCase() + ' ' + sobrenomeCliente.trim().toUpperCase();
-
-  @action
-  void increment() {
-    value++;
-  }
-}
+abstract class _AppControllerBase with Store {}
